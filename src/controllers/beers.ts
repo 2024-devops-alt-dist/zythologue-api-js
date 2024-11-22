d1f2763ba28015e78f414a02df6c34818863a23f
Juliette Suc <juliette.suc@outlook.fr>
2024-11-22 16:52:26 +0100

    feat/get beers from web navigator, working connexion with database

--- src/controllers/beers.ts
+++ src/controllers/beers.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
+import { pool } from "../db/config";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const beersController = {
-    get: (req: Request, res: Response) => {
+    get:  async  (req: Request, res: Response) => {
         try {
-            // récupérer la co de la BDD
-            // const data = select * from beers
-            // const data = [];
-            res.status(200).json({ data: "ici toutes les data" });
+            const data = await pool.query('SELECT * FROM beer');
+            res.status(200).json({data : data.rows});
         } catch (error) {
             res.status(200).json({ msg: error, message: "y a une erreur" });
         }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
-import express, { Application } from "express";
-import app from "./app"
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
+import { pool } from "./db/config";
 
-const port = 3000;
+pool.connect()
+    .then(() => {
+        console.log("Connexion à PostgreSQL réussie");
 
-app.listen(port, () => {  console.log(`Server is running on http://localhost:${port}`)})
+        const PORT = 3000;
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => console.error("Erreur de connexion à PostgreSQL", err));
+
 
