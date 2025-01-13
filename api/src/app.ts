d2c8db8cf5ac11798b961fe2caf6a0532d9e4aaf
Juliette Suc <juliette.suc@outlook.fr>
2025-01-13 12:11:04 +0100

    Wip : cors install

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,13 @@
 import express, { Application } from "express";
+var cors = require('cors')
 const app: Application = express()
 import { setupSwagger } from "./swagger";
-
-
+const corsOptions = {
+    origin: ['http://localhost:3000/', 'http://localhost:5173/'], // Remplace par l'URL de ton front
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
+    allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
+  };
+app.use(cors(corsOptions))
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
